15e1967602736371cd55941f7b1c046480df8d5d
onsetsu <stefan.ramson@gmail.com>
2017-02-22 17:12:25 +0100

    SQUASHED: AUTO-COMMIT-transform-transform.js,

--- transform/transform.js
+++ transform/transform.js
@@ -4,45 +4,30 @@ var aexprCallbacks = [],
     signals = [],
     solveSignals = false,
     resolveSignals = function() {
-        if(!solveSignals) {
-            solveSignals = true;
-            signals.forEach(s => s());
-            solveSignals = false;
-            let nonSignalCB;
-            while(nonSignalCB = aexprCallbacks.pop()) {
-                nonSignalCB();
-            }
-        }
-    },
-    newAExpr = function(axp) {
-        return {
-            onChange(cb) {
-                axp.onChange(val => {
-                    if(solveSignals) {
-                        aexprCallbacks.push(() => cb(axp.getCurrentValue()));
-                    } else {
-                        return cb(val);
-                    }
-                });
-            }
-        }
+      signals.forEach(s => s ());
     }
 `);
-    var signal = template(`(aexpr(() => init).onChange(resolveSignals), signals.push(() => name = init), init)`);
-
+    
     return {
         visitor: {
             Program(program) {
-                let aexprs = new Set();
+                let assignmentExpressions = new Set();
                 program.traverse({
-                    CallExpression(path) {
-                        let callee = path.get("callee");
-                        if(callee.isIdentifier() && callee.node.name === 'aexpr')
-                            aexprs.add(path);
+                    AssignmentExpression(path) {
+                      assignmentExpressions.add(path);
+                    },
+                    UpdateExpression(path) {
+                      assignmentExpressions.add(path);
                     }
                 });
-                aexprs.forEach(path => path.replaceWith(template(`newAExpr(expr)`)({ expr: path.node })));
+                assignmentExpressions.forEach(path => {
+                  path.replaceWith(template(`(result => {
+                    resolveSignals();
+                    return result
+                  })(expr)`)({ expr: path.node }));
+                });
 
+                var signal = template(`(signals.push(() => name = init), init)`);
                 program.traverse({
                     Identifier(path) {
                         if(!path.parentPath.isVariableDeclarator()) { return; }
